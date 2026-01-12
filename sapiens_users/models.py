86847d1471167b720ac73db4d32211d0915f54ca
o-Hakal-o <hakalsthrope@gmail.com>
2026-01-12 14:52:37 -0500

    register
    making the view for the register , and correcting the user model

--- sapiens_users/models.py
+++ sapiens_users/models.py
@@ -1,22 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 Genders = [
-    ('M', 'Male'),
-    ('F', 'Female'),
+    ('M', 'Masculino'),
+    ('F', 'Femenino'),
     ]
 
 
 class User(AbstractUser):
     
+    class Role(models.TextChoices):
+        ESTUDIANTE = 'STUDENT' , 'Estudiante'
+        PROFESOR = 'PROFESSOR' , 'Profesor'
+        MODERATOR = 'MODERATOR' , 'Moderador'
+    
+    rol = models.CharField(
+        max_length= 20,
+        choices = Role.choices,
+        default = Role.ESTUDIANTE,
+    )
     id = models.BigAutoField(primary_key = True)
     Nombre_de_Usuario  = models.CharField(max_length = 24)
-    Alias = models.CharField(max_length = 14)
+    Alias = models.CharField(max_length = 14 , blank= True , null = True)
     Avatar = models.ImageField(upload_to = 'media/avatars' , null = True , blank = True) 
     Sexo = models.CharField(max_length= 1 , choices = Genders)
     Nota = models.CharField(max_length = 240)
     Fecha_nacimiento = models.DateField()
+    gmail = models.EmailField()
     Contrasenna = models.TextField(max_length = 24 , blank = True )
     
     def __str__(self):
-        return self.name
+        return f"{self.Nombre_de_Usuario} - {self.rol}"
     
