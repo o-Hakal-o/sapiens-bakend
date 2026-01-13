4de0a19f0bb24ca728f4c86440522c734ba1ca2f
joseluka2290-maker <hakalsthrope@gmail.com>
2026-01-13 17:57:20 -0500

    fixing serializers.py
    xd

--- sapiens_users/serializers.py
+++ sapiens_users/serializers.py
@@ -16,29 +16,31 @@ class StudentRegisterSerializer(serializers.ModelSerializer):
         fields = ['Nombre_de_Usuario', 'gmail', 'password', 'username']
 
     def create(self, validated_data):
-    # 1. Extraemos el password del diccionario
-     raw_password = validated_data.get('password')
-    
-    # 2. Aseguramos los otros campos obligatorios
-    if not validated_data.get('username'):
-        validated_data['username'] = validated_data.get('gmail')
-    validated_data['rol'] = User.Role.ESTUDIANTE
+        # 1. Extraemos el password del diccionario
+        raw_password = validated_data.get('password')
+        
+        # 2. Aseguramos el username
+        if not validated_data.get('username'):
+            validated_data['username'] = validated_data.get('gmail')
+            
+        validated_data['rol'] = User.Role.ESTUDIANTE
 
-    # 3. Creamos el usuario pasando explícitamente 'Contraseña'
-    # Así, cuando Django hace el INSERT en SQL, la columna ya lleva el valor.
-    user = User.objects.create_user(
-        Contraseña=raw_password, 
-        **validated_data
-    )
-    
-    return user
-    
+        # 3. Creamos el usuario pasando explícitamente 'Contraseña'
+        # para evitar el error de nulidad en tu base de datos.
+        user = User.objects.create_user(
+            Contraseña=raw_password, 
+            **validated_data
+        )
+        
+        # IMPORTANTE: Este return debe estar alineado dentro de la función create
+        return user
+
 class LoginSerializer(serializers.Serializer):
     gmail = serializers.EmailField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        # Importante: authenticate usa el USERNAME_FIELD definido en el modelo
+        # authenticate usa el USERNAME_FIELD definido en el modelo (gmail)
         user = authenticate(username=data.get('gmail'), password=data.get('password'))
         if user and user.is_active:
             return user
