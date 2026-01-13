ea13c34d2e51a3d8e21eb6b3a000cf8fbfa3860f
joseluka2290-maker <hakalsthrope@gmail.com>
2026-01-13 16:57:26 -0500

    finishing the instalation of Corseheaders
    xd

--- sapiens_core/settings.py
+++ sapiens_core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'sapiens_users',
+    'corsheaders',
     'sapiens_articulos',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
@@ -60,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
