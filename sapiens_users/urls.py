7108a58cf98d72d34421a37ed3696b4b903876a7
joseluka2290-maker <hakalsthrope@gmail.com>
2026-01-14 23:58:09 -0500

    adding the documentation urls and the  missing import here too

--- sapiens_users/urls.py
+++ sapiens_users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StudentRegisterView, LoginView, UserProfileView # <-- Asegúrate de que esté aquí
-
+from .views import StudentRegisterView, LoginView, UserProfileView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('register/student/', StudentRegisterView.as_view(), name='student-register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    #Rutas de documentación.
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
