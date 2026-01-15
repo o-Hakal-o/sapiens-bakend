fe35ed59688a3e1f48d237f6de467be330dfbd43
joseluka2290-maker <hakalsthrope@gmail.com>
2026-01-15 00:24:21 -0500

    xd

--- sapiens_users/urls.py
+++ sapiens_users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('register/student/', StudentRegisterView.as_view(), name='student-register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    
     #Rutas de documentación.
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
